0f47f3c70d09bbc2affe81db446ac134fb946504
AntonGoldobin <722451@mail.ru>
2024-04-27 16:10:50 +0300

    CONNECTED TO UNITY

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { createClient } from '@connect2ic/core'
+import { defaultProviders } from '@connect2ic/core/providers'
+import '@connect2ic/core/style.css'
+import { Connect2ICProvider } from '@connect2ic/react'
 import './App.css'
 import { Home } from './pages/Home/Home'
 
+const client = createClient({
+	providers: defaultProviders as any,
+	//globalProviderConfig: {
+	//	dev: import.meta.env.DEV,
+	//},
+})
+
 function App() {
 	return (
-		<div className="App-header">
-			<Home />
-		</div>
+		<Connect2ICProvider client={client}>
+			<div className="App-header">
+				<Home />
+			</div>
+		</Connect2ICProvider>
 	)
 }
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,18 @@
+import { ConnectButton, ConnectDialog, useConnect } from '@connect2ic/react'
 import { Unity, useUnityContext } from 'react-unity-webgl'
 
 export const Home = () => {
-	const { unityProvider } = useUnityContext({
+	const { isConnected, principal } = useConnect({
+		onConnect: () => {
+			console.log('connected')
+			sendMessage('AuthorizationPage', 'sendMessage', principal)
+		},
+		onDisconnect: () => {
+			console.log('disconected')
+		},
+	})
+
+	const { unityProvider, sendMessage } = useUnityContext({
 		loaderUrl: 'build/unity-webgl-build.loader.js',
 		dataUrl: 'build/unity-webgl-build.data',
 		frameworkUrl: 'build/unity-webgl-build.framework.js',
@@ -9,10 +20,26 @@ export const Home = () => {
 	})
 
 	return (
-		<Unity
-			unityProvider={unityProvider}
-			style={{ width: '1000px', height: '800px' }}
-		/>
+		<div style={{ display: 'flex' }}>
+			<div>
+				{principal || 'Не залогинен'}
+				<ConnectButton />
+				<ConnectDialog />
+				<button
+					onClick={() =>
+						sendMessage('AuthorizationPage', 'sendMessage', principal)
+					}
+				>
+					set message
+				</button>
+			</div>
+			<div>
+				<Unity
+					unityProvider={unityProvider}
+					style={{ width: '1000px', height: '800px' }}
+				/>
+			</div>
+		</div>
 	)
 	//return <></>
 }
